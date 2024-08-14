2b9f75e1ebef46f315c3a307cfd236d127d7906a
Aqsal Dharmaputra <aqsaldpa28@gmail.com>
2024-08-14 21:00:58 +0700

    Initial Commit

---
+++ firstSwiftApps/firstSwiftAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftAppsApp.swift
+//  firstSwiftApps
+//
+//  Created by Aqsal Dharmaputra on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
